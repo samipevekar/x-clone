b01b3c999507b78de86cd530456b584d7efbefb6
samipevekar <samipevekar499@gmail.com>
2024-08-09 14:47:51 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express"
 import connectMongoDB from './db/connectMongodb.js';
 import cookieParser from 'cookie-parser';
 import {v2 as cloudinary} from "cloudinary"
+import cors from "cors"
 
 import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
@@ -21,10 +22,22 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000
 
+app.use(cors({
+    origin:"*",
+    methods:["GET","POST","DELETE","PUT","OPTIONS"],
+    credentials:true
+}))
+
 app.use(express.json({limit:"5mb"}))        // to parse req.body
 app.use(express.urlencoded({extended:true}))  // to parse form data
 
 app.use(cookieParser())
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+
 
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
